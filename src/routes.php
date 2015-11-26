601466387d4d09c7eeaaf0a34c8e8c7d9fcb00fa
CEM ADILOGLU <cemadil@gmail.com>
2015-11-26 12:45:54 +0200

    route fix

--- src/routes.php
+++ src/routes.php
@@ -3,7 +3,7 @@
 Route::group(array('middleware' => ['acl:ViewLog']), function()
 {
 	Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
-}
+});
 
 Route::group(array('middleware' => ['auth']), function()
 {
